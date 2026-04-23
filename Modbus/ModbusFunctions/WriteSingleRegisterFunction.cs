631362f8ea283191c2ce85ad5b8be645c354f684
Matija Brkusanin <brkusaninmatija@gmail.com>
2026-04-23 16:47:36 +0200

    ReadCoilsFunction.cs -> PackRequest i ParseResponse odranjene
    ReadHoldingRegistersFunction.cs -> PackRequest i ParseResponse odranjene
    
    ReadInputRegistersFunction.cs -> PackRequest i ParseResponse odranjene
    
    WriteSingleCoilFunction.cs -> PackRequest i ParseResponse odranjene
    
    WriteSingleRegisterFunction.cs -> PackRequest i ParseResponse odranjene

--- Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -24,15 +24,49 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+
+            request[0] = (byte)(p.TransactionId >> 8);
+            request[1] = (byte)(p.TransactionId & 0xFF);
+
+            request[2] = (byte)(p.ProtocolId >> 8);
+            request[3] = (byte)(p.ProtocolId & 0xFF);
+
+            request[4] = (byte)(p.Length >> 8);
+            request[5] = (byte)(p.Length & 0xFF);
+
+            request[6] = p.UnitId;
+            request[7] = p.FunctionCode;
+
+            request[8] = (byte)(p.OutputAddress >> 8);
+            request[9] = (byte)(p.OutputAddress & 0xFF);
+
+            request[10] = (byte)(p.Value >> 8);
+            request[11] = (byte)(p.Value & 0xFF);
+
+            return request;
         }
 
-        /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> retVal = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            byte functionCode = response[7];
+
+            if (functionCode == (byte)(p.FunctionCode + 0x80))
+            {
+                HandeException(response[8]);
+            }
+
+            ushort address = (ushort)((response[8] << 8) | response[9]);
+            ushort value = (ushort)((response[10] << 8) | response[11]);
+
+            retVal.Add(Tuple.Create(PointType.ANALOG_OUTPUT, address), value);
+
+            return retVal;
         }
     }
 }
